526de304062e5f9a58571caf193eaf21ffb2e70b
liuyang_glb <786724523_glb@qq.com>
2020-03-08 14:45:35 +0800

    更改了，我是猪猪猪的

--- src/main/java/pers/ly/typestring/StringOne.java
+++ src/main/java/pers/ly/typestring/StringOne.java
@@ -4,7 +4,7 @@ package pers.ly.typestring;
 
 public class StringOne {
     public static void main(String[] args) {
-        System.out.println("你好呀，我是第一次");
+        System.out.println("我也修改了，我是主的");
     }
 
     public static String change(String s ){
