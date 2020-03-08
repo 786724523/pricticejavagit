ea857f6c73509edccf4ebc08c97bd53b2b790c42
liuyang <786724523@qq.com>
2020-03-08 14:59:15 +0800

    安安

--- src/main/java/pers/ly/typestring/StringOne.java
+++ src/main/java/pers/ly/typestring/StringOne.java
@@ -5,6 +5,7 @@ package pers.ly.typestring;
 public class StringOne {
     public static void main(String[] args) {
         System.out.println("我也修改了，我是主的");
+        System.out.println(12312313);
     }
 
     public static String change(String s ){
