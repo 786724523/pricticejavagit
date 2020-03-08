70c491a13e0abc79d440620b4873cffccf90b6a6
刘洋 <786724523@qq.com>
2020-03-08 16:27:34 +0800

    123

--- src/main/java/pers/ly/typestring/StringOne.java
+++ src/main/java/pers/ly/typestring/StringOne.java
@@ -4,7 +4,7 @@ package pers.ly.typestring;
 
 public class StringOne {
     public static void main(String[] args) {
-        System.out.println("我也修改了，我是主的");
+        System.out.println("我也修改了，我是主主张租住的");
     }
 
     public static String change(String s ){
