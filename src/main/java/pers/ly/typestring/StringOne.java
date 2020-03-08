0a2a02f61c49efa59e3fa01624e1481c8a1686b5
liuyang_glb <786724523_glb@qq.com>
2020-03-08 14:26:07 +0800

    加了123

--- src/main/java/pers/ly/typestring/StringOne.java
+++ src/main/java/pers/ly/typestring/StringOne.java
@@ -5,6 +5,7 @@ package pers.ly.typestring;
 public class StringOne {
     public static void main(String[] args) {
         System.out.println("你好呀，我是第一次");
+        System.out.println(123);
     }
 
     public static String change(String s ){
